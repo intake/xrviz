cc635e52aadf8ac81c7e5766fc57a4d66581f858
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-01 15:12:51 +0530

    update data file path

--- examples/display_section.py
+++ examples/display_section.py
@@ -1,7 +1,7 @@
 import xarray as xr
 from xrviz.display.section import Display
 
-ds = xr.open_dataset("/home/hdsingh/Desktop/xrviz/xrviz/sample_data/great_lakes.nc");
+ds = xr.open_dataset("../xrviz/sample_data/great_lakes.nc")
 
 display = Display(ds)
 display.show()
