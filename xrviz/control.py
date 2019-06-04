f3453bab8c4115e3d86688589f669560ad8e71b3
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-04 20:57:03 +0530

    pre set dataset properties

--- xrviz/control.py
+++ xrviz/control.py
@@ -22,3 +22,13 @@ class Control(SigSlot):
         selected_property = self.displayer.selected_property
         sub_property = self.displayer.selected_subproperty
         self.describer.setup(selected_property, sub_property)
+
+    def set_dataset_property(self, selected_property):
+        if isinstance(self.data, xr.Dataset):
+            if selected_property in list(self.displayer.properties):
+                selected_property = f'{self.displayer.right} {selected_property}'
+                self.displayer.select.value = [selected_property]
+            else:
+                print(f"Use one of {list(self.displayer.properties)}")
+        else:
+            print("This method is applicable only for xr.Dataset")
