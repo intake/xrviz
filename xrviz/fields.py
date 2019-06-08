05c09c0cf15381f251f27d21a7f5650e82a19274
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-08 10:26:50 +0530

    options separation for x and y

--- xrviz/fields.py
+++ xrviz/fields.py
@@ -11,6 +11,10 @@ class Fields(SigSlot):
         self.x = pn.widgets.Select(name='x')
         self.y = pn.widgets.Select(name='y')
 
+        self._register(self.x, 'x')
+
+        self.connect('x', self.change_y)
+
         self.panel = pn.Column(self.x, self.y, name='fields',)
 
     def set_data(self, data):
@@ -19,9 +23,25 @@ class Fields(SigSlot):
 
     def setup(self, var):
         self.var = var[0]
-        coords = [coord for coord in self.data[var].coords.keys()]
-        self.x.options = coords
-        self.y.options = coords
+        self.var_coords = [coord for coord in self.data[var].coords.keys()]
+        x_opts = self.var_coords.copy()
+        self.x.options = x_opts
+        self.x.value = x_opts[0]
+        y_opts = x_opts.copy()
+        del y_opts[0]
+        if y_opts is None:
+            self.y.options = []
+        else:
+            self.y.options = y_opts
+
+    def change_y(self, value):
+        _opts = self.var_coords.copy()
+        values = _opts.copy()
+        values.remove(self.x.value)
+        if len(values) == 0:
+            self.y.options = []
+        else:
+            self.y.options = values
 
     @property
     def kwargs(self):
