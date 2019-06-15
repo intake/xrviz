5c4de1f4731b56014d57b208bd557336459194ff
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-15 14:01:27 +0530

    Mean, max, min, median and clear agg graph

--- xrviz/fields.py
+++ xrviz/fields.py
@@ -24,29 +24,34 @@ class Fields(SigSlot):
     def __init__(self, data):
         super().__init__()
         self.data = data
-        self.x = pn.widgets.Select(name='x')
-        self.y = pn.widgets.Select(name='y')
-        self.dims_selector = pn.widgets.MultiSelect(size=8, min_width=150,
+        self.x = pn.widgets.Select(name='x', width=200)
+        self.y = pn.widgets.Select(name='y', width=200)
+        self.aggregation = pn.widgets.Select(name='Aggregation',
+                                             width=200,
+                                             options=['mean', 'max',
+                                                      'min', 'median'])
+        self.dims_selector = pn.widgets.MultiSelect(size=8, min_width=200,
                                                     height=110,
                                                     width_policy='min',
                                                     name='Aggregate along')
-        self.agg_plot = pn.widgets.Button(name='Aggregate',
-                                          width=150,
-                                          disabled=True)
+        self.agg_plot_button = pn.widgets.Button(name='Aggregate',
+                                                 width=200,
+                                                 disabled=True)
+        self.agg_graph = pn.Row(pn.Spacer(name='Agg Graph'))
 
         self._register(self.x, 'x')
         self._register(self.dims_selector, 'agg_dims')
-        self._register(self.agg_plot, 'agg_plot', 'clicks')
+        self._register(self.agg_plot_button, 'agg_plot_button', 'clicks')
 
         self.connect('x', self.change_y)
         self.connect('agg_dims', self.setup_agg_button)
-        self.connect('agg_plot', self.create_agg_plot)
+        self.connect('agg_plot_button', self.create_agg_plot)
 
         self.panel = pn.Row(pn.Column(self.x, self.y),
-                            # pn.Spacer(width=200),
-                            pn.Column(self.dims_selector,
-                                      self.agg_plot,),
-                            pn.Spacer(name='Graph'),
+                            pn.Column(self.aggregation,
+                                      self.dims_selector,
+                                      self.agg_plot_button),
+                            self.agg_graph,
                             name='Fields',)
 
         if isinstance(data, xr.DataArray):
@@ -55,7 +60,8 @@ class Fields(SigSlot):
     def setup(self, var):
         self.dims_selector.value = []  # To empty previouly selected value from selector
         self.dims_selected_for_agg = []
-        self.agg_plot.disabled = True
+        self.agg_plot_button.disabled = True
+        self.agg_graph[0] = pn.Spacer(name='Agg Graph')  # To clear Agg Graph upon selection of new variable
         if isinstance(self.data, xr.Dataset):
             self.var = var[0]
             self.var_dims = list(self.data[var].dims)
@@ -76,11 +82,11 @@ class Fields(SigSlot):
 
     def setup_agg_button(self, selected_dims):
         self.dims_selected_for_agg = selected_dims
-        self.agg_plot.disabled = True
+        self.agg_plot_button.disabled = True
         # We need atleast 1 and at max (dims-1) selections to aggregate
         # So this button would be enbled when this condition is satisfied.
         if len(selected_dims) == 1 or len(selected_dims) < len(self.dims_selector.options):
-            self.agg_plot.disabled = False
+            self.agg_plot_button.disabled = False
 
     def change_y(self, value):
         """
@@ -94,15 +100,26 @@ class Fields(SigSlot):
     def create_agg_plot(self, *args):
         remaining_dims = [dim for dim in self.var_dims if dim not in self.dims_selected_for_agg]
         assign_opts = {dim: self.data[dim] for dim in remaining_dims}
-        sel = self.data[self.var].mean(self.dims_selected_for_agg)
+
+        # the method getattr(data.air_u, self.aggregation.value ,[*self.dims_selected_for_agg])
+        # does not works as expected
+        if self.aggregation.value == 'mean':
+            sel = self.data[self.var].mean(self.dims_selected_for_agg)
+        elif self.aggregation.value == 'max':
+            sel = self.data[self.var].max(self.dims_selected_for_agg)
+        elif self.aggregation.value == 'min':
+            sel = self.data[self.var].min(self.dims_selected_for_agg)
+        elif self.aggregation.value == 'median':
+            sel = self.data[self.var].median(self.dims_selected_for_agg)
+
         sel = sel.assign_coords(**assign_opts)
 
         # for 1d sel we have simple hvplot
         # for 2d or 3d we will have quadmesh
         if len(sel.shape) == 1:
-            self.panel[-1] = sel.hvplot()
+            self.agg_graph[0] = sel.hvplot()
         else:
-            self.panel[-1] = sel.hvplot.quadmesh()
+            self.agg_graph[0] = sel.hvplot.quadmesh()
 
     @property
     def kwargs(self):
