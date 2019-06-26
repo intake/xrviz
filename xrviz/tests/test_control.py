3af150c1ba4298897a05db09bd89b043b2c0bf0b
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-26 11:30:21 +0530

    Correct Tests

--- xrviz/tests/test_control.py
+++ xrviz/tests/test_control.py
@@ -10,4 +10,4 @@ def control(data):
 
 def test_control_initial(control):
     tabs = [tab.name for tab in control.tabs]
-    assert tabs == ['Fields', 'Set Coords']
+    assert tabs == ['Fields', 'Projection', 'Set Coords']
