749841ec8a6053bbe2b1f66ad6e774c028e0e258
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-30 06:28:03 +0530

    xrviz.example() works now

--- xrviz/__init__.py
+++ xrviz/__init__.py
@@ -3,3 +3,11 @@ from . import sample_data
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+def example():
+    import xarray as xr
+    from . import dashboard
+    ds = sample_data.great_lakes
+    dash = dashboard.Dashboard(ds)
+    dash.show()
