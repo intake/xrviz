4330a6f3c9f48db10d6c4bc1735d1f38229263ae
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-30 19:20:33 +0530

    Sample data in example

--- xrviz/__init__.py
+++ xrviz/__init__.py
@@ -1,5 +1,3 @@
-from . import sample_data
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
@@ -20,6 +18,7 @@ def example(show=True):
             ```
     """
     import xarray as xr
+    from . import sample_data
     from . import dashboard
     ds = sample_data.great_lakes
     dash = dashboard.Dashboard(ds)
