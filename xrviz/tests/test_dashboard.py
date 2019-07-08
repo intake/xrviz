a2918126456d80ed5efaa9fe7e49b469ebe31de8
hdsingh <harmandeepsinghhds@gmail.com>
2019-07-08 11:29:24 +0530

    Test set_coords, link_agg_selectors, animate, invalid_dtype in dashboard

--- xrviz/tests/test_dashboard.py
+++ xrviz/tests/test_dashboard.py
@@ -16,6 +16,11 @@ def dash_for_Array(data):
     return Dashboard(data.temp)
 
 
+def test_dashboard_with_invalid_datatype():
+    with pytest.raises(ValueError):
+        Dashboard(str)
+
+
 def test_check_is_plottable_1D(dashboard):
     # `validtime` is 1D variable
     dashboard.plot_button.disabled = False
@@ -37,7 +42,7 @@ def test_check_is_plottable_other_vars(dashboard):
     assert dashboard.plot_button.disabled is False
 
 
-def test_2d_variable(dashboard):
+def test_2d_variable_for_dims(dashboard):
     dashboard.control.displayer.select_variable('lat')
     fields = dashboard.control.fields
     assert fields.x.value == 'nx'
@@ -49,7 +54,7 @@ def test_2d_variable(dashboard):
     assert [index_sel for index_sel in dashboard.output[1]] == []
 
 
-def test_3d_variable(dashboard):
+def test_3d_variable_for_dims(dashboard):
     dashboard.control.displayer.select_variable('air_v')
     fields = dashboard.control.fields
     assert fields.x.value == 'nx'
@@ -64,9 +69,9 @@ def test_3d_variable(dashboard):
 
 
 @pytest.mark.parametrize('dashboard',
-                         ['dashboard','dash_for_Array'],
+                         ['dashboard', 'dash_for_Array'],
                          indirect=True)
-def test_4d_variable(dashboard):
+def test_4d_variable_for_dims(dashboard):
     dashboard.control.displayer.select_variable('temp')
     fields = dashboard.control.fields
     assert fields.x.value == 'nx'
@@ -78,3 +83,31 @@ def test_4d_variable(dashboard):
     assert isinstance(graph, pn.pane.holoviews.HoloViews)
     index_selectors = [index_sel.name for index_sel in dashboard.output[1]]
     assert index_selectors == ['sigma', 'time']
+
+
+def test_link_aggregation_selectors(dashboard):
+    dashboard.control.displayer.select_variable('temp')
+    llim = dashboard.control.style.lower_limit
+    ulim = dashboard.control.style.upper_limit
+    llim.value = '10'
+    ulim.value = '20'
+    agg_sel = dashboard.control.fields.agg_selectors[0]
+    agg_sel.value = 'mean'
+    assert llim.value is None
+    assert ulim.value is None
+
+
+def test_set_coords(dashboard):
+    new_coords = sorted(['time', 'sigma', 'lat', 'lon'])
+    dashboard.control.coord_setter.coord_selector.value = new_coords
+    assert sorted(list(dashboard.data.coords)) == new_coords
+
+
+def test_animate_wigdet_for_dims(dashboard):
+    dashboard.data.reset_coords()
+    dashboard.control.displayer.select_variable('temp')
+    fields = dashboard.control.fields
+    agg_selectors = fields.agg_selectors
+    agg_selectors[0].value = 'animate'
+    dashboard.create_plot()
+    isinstance(dashboard.output[1][0][1], pn.widgets.player.DiscretePlayer)
