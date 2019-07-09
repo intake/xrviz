a91e5dfc1c403d6e91463a805893ef781ec3e4d9
hdsingh <harmandeepsinghhds@gmail.com>
2019-07-09 12:58:35 +0530

    Tests for Fields

--- xrviz/tests/test_fields.py
+++ xrviz/tests/test_fields.py
@@ -1,6 +1,8 @@
 import pytest
 from xrviz.fields import Fields
 from . import data
+from .test_dashboard import dashboard
+from ..compatibility import mpcalc
 
 
 @pytest.fixture()
@@ -22,3 +24,38 @@ def test_fields_initial(fields):
     assert fields.panel.name == 'Axes'
     assert fields.panel[0][0].object == '### Plot Dimensions'
     assert fields.panel[1][0].object == '### Aggregations'
+
+
+def test_x_y_var_without_coords(dashboard):
+    dashboard.control.displayer.select_variable('temp')
+    assert dashboard.control.fields.x.value == 'nx'
+    assert dashboard.control.fields.y.value == 'ny'
+
+
+def test_x_y_var_with_coords(dashboard):
+    dashboard.control.displayer.select_variable('temp')
+    dashboard.control.coord_setter.coord_selector.value = ['lat', 'lon']
+    x_val, y_val = ('lon', 'lat') if mpcalc else ('lat', 'lon')
+    assert dashboard.control.fields.x.value == x_val
+    assert dashboard.control.fields.y.value == y_val
+
+
+def test_change_y_dim_selectors(dashboard):
+    dashboard.control.coord_setter.coord_selector.value = ['time', 'sigma']
+    dashboard.control.displayer.select_variable('temp')
+    fields = dashboard.control.fields
+    fields.x.value = 'time'
+    assert fields.y.value == 'nx'
+    assert fields.agg_selectors[0].name == 'ny'
+    assert fields.agg_selectors[1].name == 'sigma'
+
+
+def test_check_are_var_coords(dashboard):
+    fields = dashboard.control.fields
+    fields.x.value = 'nx'
+    fields.y.value = 'ny'
+    assert fields.check_are_var_coords() is False
+    dashboard.control.coord_setter.coord_selector.value = ['lat', 'lon']
+    fields.x.value = 'lon'
+    fields.y.value = 'lat'
+    assert fields.check_are_var_coords() is True
