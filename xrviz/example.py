361d7cb6ab049b6d390db2e0421d6d854b2787be
hdsingh <harmandeepsinghhds@gmail.com>
2019-07-12 23:19:39 +0530

    Correct example

--- xrviz/example.py
+++ xrviz/example.py
@@ -15,14 +15,25 @@ def example(show=True):
     from .sample_data import great_lakes
 
     great_lakes = great_lakes.set_coords(['lat', 'lon'])
-    initial_params = {'projection_ops': {'is_geo': True,
-                                         'projection': 'Orthographic',
-                                         'proj_params': {'central_longitude': -78, 
-                                                         'central_latitude': 43}},
-                      # 'style_ops': {'height': 400,
-                      #               'width': 800}
-                      }
+    initial_params = {'Variables': 'temp',
+                      'Set Coords': ['lat', 'lon'],
+                      'x': 'lon',
+                      'y': 'lat',
+                      'sigma': 'animate',
 
+                      # style
+                      'height': 400,
+                      'width': 700,
+                      'colorbar': False,
+                      # projection
+
+                      'is_geo': True,
+                      'crs': 'PlateCarree',
+                      'projection': 'Orthographic',
+                      'crs params': {'central_longitude': 0.0},
+                      'projection params': {'central_longitude': -78,
+                                            'central_latitude': 43,
+                                            'globe': None}}
     dash = Dashboard(great_lakes, initial_params=initial_params)
     if show:
         dash.show()
