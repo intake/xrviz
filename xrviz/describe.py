0810f354c42177871dca06a35b71cf02f6d2c7d9
Julia Signell <jsignell@gmail.com>
2019-10-25 10:29:34 -0400

    Using repr_html for describe

--- xrviz/describe.py
+++ xrviz/describe.py
@@ -31,51 +31,13 @@ class Describe(SigSlot):
     """
     def __init__(self, data):
         super().__init__()
+        xr.set_options(display_style='html')
         self.data = data
-        self.panel = pn.pane.HTML(
-            style={'font-size': '12pt'}, min_width=800, height=100)
+        self.panel = pn.pane.HTML(min_width=500, height=400, css_classes=['xrviz-scroll'])
         self.panel.object = "Description Section"
-        self._template_load_path = os.path.join(os.path.dirname(__file__),
-                                                "templates")
-        self._template_env = Environment(loader=FileSystemLoader(self._template_load_path))
-        self._variable_template = self._template_env.get_template('variable.html')
 
     def setup(self, var):
-        self.panel.object = self.variable_pane_for_dataset(var)
-
-    def variable_pane_for_dataset(self, var):
-        """
-        Returns HTML template describing variable and global attributes.
-        """
-        if var is not None:
-            var = var if isinstance(var, str) else var[0]
-            var_attrs = [(k, v) for k, v in self.data[var].attrs.items()]
-            var_coords = [coord for coord in self.data[var].coords]
-            var_dims = self.data[var].dims
-            var_dtype = str(self.data[var].dtype)
-            var_name = self.data[var].name
-            var_nbytes = self.data[var].nbytes
-            var_shape = self.data[var].shape
-            var_size = self.data[var].size
-            var_dim_shape = [(dim, shape) for dim, shape in zip(var_dims, var_shape)]
-            data_attrs = [(k, v) for k, v in self.data.attrs.items()]
-            data_coords = [coord for coord in self.data.coords.keys()]
-            data_dims = [(k, v) for k, v in self.data.dims.items()]
-
-            return self._variable_template.render(data_attrs=data_attrs,
-                                                  data_coords=data_coords,
-                                                  data_dims=data_dims,
-                                                  var=var,
-                                                  var_attrs=var_attrs,
-                                                  var_coords=var_coords,
-                                                  var_dim_shape=var_dim_shape,
-                                                  var_dtype=var_dtype,
-                                                  var_name=var_name,
-                                                  var_nbytes=var_nbytes,
-                                                  var_size=var_size,
-                                                  )
-        else:
-            return self._variable_template.render(var=None)
+        self.panel.object = self.data
 
     def set_coords(self, data, var):
         self.data = data
