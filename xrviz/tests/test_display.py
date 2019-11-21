fba0d8d6f6889de16635a181a8b5e0f5a4c3d719
Julia Signell <jsignell@gmail.com>
2019-11-21 12:33:02 -0500

    Fixing tests for autocomplete

--- xrviz/tests/test_display.py
+++ xrviz/tests/test_display.py
@@ -13,38 +13,27 @@ def displayer(data):
 @pytest.fixture()
 def displayer_array(data):
     data = data.temp
-    data = xr.Dataset({f'{data.name}': data}, attrs=data.attrs) 
+    data = xr.Dataset({f'{data.name}': data}, attrs=data.attrs)
     return Display(data)
 
 
 @pytest.fixture()
 def displayer_array_single_var(data):
     data = data.lon
-    data = xr.Dataset({f'{data.name}': data}, attrs=data.attrs) 
+    data = xr.Dataset({f'{data.name}': data}, attrs=data.attrs)
     return Display(data)
 
 
 def test_fill_items(displayer):
-    assert displayer.select.options == {'validtime': 'validtime',
-                                        'time 📈': 'time',
-                                        'lon': 'lon',
-                                        'lat': 'lat',
-                                        'mask': 'mask',
-                                        'depth': 'depth',
-                                        'sigma 📈': 'sigma',
-                                        'zeta': 'zeta',
-                                        'air_u': 'air_u',
-                                        'air_v': 'air_v',
-                                        'u': 'u',
-                                        'v': 'v',
-                                        'temp': 'temp'}
+    assert displayer.select.options == [
+        'validtime', 'time', 'lon', 'lat', 'mask', 'depth',
+        'sigma', 'zeta', 'air_u', 'air_v', 'u', 'v', 'temp']
 
 
 def test_fill_items_array(displayer_array):
-    assert displayer_array.select.options == {'time 📈': 'time',
-                                              'sigma 📈': 'sigma',
-                                              'temp': 'temp'}
+    assert displayer_array.select.options == [
+        'time', 'sigma', 'temp']
 
 
 def test_fill_items_single_var(displayer_array_single_var):
-    assert displayer_array_single_var.select.options == {'lon': 'lon'}
+    assert displayer_array_single_var.select.options == ['lon']
